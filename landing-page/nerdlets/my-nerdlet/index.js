c75a2cf808506cebbceb38b19fe8082a2f1cee9f
Amine Benzaied <abenzaied@newrelic.com>
2019-07-31 15:26:44 -0700

    Merge changes

--- landing-page/nerdlets/my-nerdlet/index.js
+++ landing-page/nerdlets/my-nerdlet/index.js
@@ -2,23 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchHeader from './SearchHeader';
 
-import SearchFilter from './search-filter.js';
-
 export default class MyNerdlet extends React.Component {
 	static propTypes = {
 		width: PropTypes.number,
 		height: PropTypes.number,
-	};
+    };
+    
 
-<<<<<<< HEAD
     render() {
-    return <SearchHeader/> 
+        return <SearchHeader/> 
     }
-=======
-	render() {
-		return (
-			<SearchFilter />
-		);
-	}
->>>>>>> 32352c1d03e84cf4d8a1db7672e20e2e8901d79b
 }
