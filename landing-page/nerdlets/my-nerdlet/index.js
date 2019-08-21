9599dd66588f70aaaf52cc7958912a4e4d29d341
Amine Benzaied <abenzaied@newrelic.com>
2019-08-21 12:47:26 -0700

    change props

--- landing-page/nerdlets/my-nerdlet/index.js
+++ landing-page/nerdlets/my-nerdlet/index.js
@@ -10,6 +10,11 @@ export default class MyNerdlet extends React.Component {
     
 
     render() {
-        return <SearchHeader/> 
+        const data = {env: [{environment:"lolriot"},{environment:"lolriotdev"},{environment:"lolriotQa"},{environment:"dev"}],
+        dataCenter: [{dcenter:"pdx2"}, {dcenter:"mia1"}, {dcenter:"euc1"}],
+        logicalCluster: [{lcluster:"prod"},{lcluster:"na1"},{lcluster:"br1"},{lcluster:"la2"}],
+        group: [{group:"platform"}, {group:"cap"}, {group:"missions"}],
+        name: [{name:"wallets"}, {name:"platform-war"}, {name:"connect2id"}]}
+        return <SearchHeader data={data}/>
     }
 }
